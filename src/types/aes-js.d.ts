83ca6a823da3a846d5d8631c26422127f981b749
milanh34 <milanh344@gmail.com>
2025-10-23 12:30:00 +0530

    Types added for aes-js

---
+++ src/types/aes-js.d.ts
@@ -0,0 +1,47 @@
+declare module 'aes-js' {
+  export namespace utils {
+    namespace utf8 {
+      function toBytes(text: string): number[];
+      function fromBytes(bytes: number[] | Uint8Array): string;
+    }
+    namespace hex {
+      function toBytes(hex: string): number[];
+      function fromBytes(bytes: number[] | Uint8Array): string;
+    }
+  }
+
+  export class Counter {
+    constructor(initialValue: number[] | Uint8Array);
+  }
+
+  export namespace ModeOfOperation {
+    class ctr {
+      constructor(key: number[] | Uint8Array, counter: Counter);
+      encrypt(plaintext: number[] | Uint8Array): Uint8Array;
+      decrypt(ciphertext: number[] | Uint8Array): Uint8Array;
+    }
+    class cbc {
+      constructor(key: number[] | Uint8Array, iv: number[] | Uint8Array);
+      encrypt(plaintext: number[] | Uint8Array): Uint8Array;
+      decrypt(ciphertext: number[] | Uint8Array): Uint8Array;
+    }
+    class cfb {
+      constructor(key: number[] | Uint8Array, iv: number[] | Uint8Array, segmentSize?: number);
+      encrypt(plaintext: number[] | Uint8Array): Uint8Array;
+      decrypt(ciphertext: number[] | Uint8Array): Uint8Array;
+    }
+    class ofb {
+      constructor(key: number[] | Uint8Array, iv: number[] | Uint8Array);
+      encrypt(plaintext: number[] | Uint8Array): Uint8Array;
+      decrypt(ciphertext: number[] | Uint8Array): Uint8Array;
+    }
+    class ecb {
+      constructor(key: number[] | Uint8Array);
+      encrypt(plaintext: number[] | Uint8Array): Uint8Array;
+      decrypt(ciphertext: number[] | Uint8Array): Uint8Array;
+    }
+  }
+
+  export function padding_pkcs7_pad(data: number[] | Uint8Array): Uint8Array;
+  export function padding_pkcs7_strip(data: number[] | Uint8Array): Uint8Array;
+}
